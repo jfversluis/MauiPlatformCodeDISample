47214ad70633c2af3d6160ba6f8a15c08f547726
Gerald Versluis <gerald@verslu.is>
2021-12-13 11:43:47 +0100

    Initial commit

---
+++ MauiPlatformCodeDISample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiPlatformCodeDISample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiPlatformCodeDISample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiPlatformCodeDISample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
